558099759290c3e978e3b1e82e007af5e2ff294d
Kris Gray <kgray@justise.com>
2020-01-04 15:26:25 -0800

    serverless

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([
-    [optimizedImages, {}],
-    [withImages, {}],
-    [withCSS, {}],
-]);
+module.exports = withPlugins(
+    [
+        [optimizedImages, {}],
+        [withImages, {}],
+        [withCSS, {}],
+    ],
+    {
+        target: 'serverless',
+    }
+);
