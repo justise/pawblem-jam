99f551a1fc314a0dae81f34ce8c2b00e57a93be3
Kris Gray <kgray@justise.com>
2024-09-29 12:51:39 -0700

    Merge conflict

---
+++ next-env.d_BACKUP_18386.ts
@@ -0,0 +1,13 @@
+/// <reference types="next" />
+<<<<<<< HEAD
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
+=======
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+>>>>>>> cd0afe5 (All broken)
